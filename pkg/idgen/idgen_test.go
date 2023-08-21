ce7a9b9d70e12b7d2ed62cb057616f34ee2844e4
Alrt.Lin <alrt0828@gmail.com>
2023-08-21 10:30:17 +0800

    clear

--- pkg/idgen/idgen_test.go
+++ pkg/idgen/idgen_test.go
@@ -1,7 +1,6 @@
 package idgen
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"strconv"
 	"strings"
@@ -9,12 +8,6 @@ import (
 	"time"
 )
 
-func TestNexta(t *testing.T) {
-	name := "王禹衡"
-
-	//s := []rune(name)[0]
-	println(string(name[2:]))
-}
 func TestNext(t *testing.T) {
 	t.Run("generate id", func(t *testing.T) {
 		m := make(map[int64]bool, 0)
@@ -37,14 +30,5 @@ func TestNext(t *testing.T) {
 func TestUid(t *testing.T) {
 	t.Run("generate uid", func(t *testing.T) {
 		assert.True(t, strings.HasPrefix(strconv.Itoa(int(Uid())), "2521"))
-	})
-}
-
-func TestCid(t *testing.T) {
-	t.Run("generate cid", func(t *testing.T) {
-		for i := 0; i < 500; i++ {
-			fmt.Println(CharacterID())
-			time.Sleep(10 * time.Millisecond)
-		}
 	})
 }
